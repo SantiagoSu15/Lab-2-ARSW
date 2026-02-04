10eefa2759d4524980f74327340abd9a61b997b1
SantiagoSu15 <sasupu08@gmail.com>
2026-02-04 10:43:20 -0500

    first

---
+++ src/main/java/co/eci/snake/app/Main.java
@@ -0,0 +1,12 @@
+package co.eci.snake.app;
+
+import co.eci.snake.ui.legacy.SnakeApp;
+
+public final class kMain {
+  private Main() {}
+  public static void main(String[] args) {
+    SnakeApp.launch();
+  }
+}
+
+ 
